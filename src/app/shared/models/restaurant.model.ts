a117da0f02db6fa5913e80d9ece5bf0fb9e8f84c
ehamed <ehamed@ejada.com>
2025-06-25 14:30:03 +0300

    Added extra restaurant details

--- src/app/shared/models/restaurant.model.ts
+++ src/app/shared/models/restaurant.model.ts
@@ -2,4 +2,13 @@ export interface Restaurant {
   id: number;
   name: string;
   imagePath: string;
+  slogan: string;
+  minimumOrderPrice: number;
+  deliveryTimeMin: number;
+  deliveryTimeMax: number; 
+  raring: number;
+  numReviews: number;
+  closingTime: string;
+  menuOptions: Array<string>;
+  offers: []; 
 }
