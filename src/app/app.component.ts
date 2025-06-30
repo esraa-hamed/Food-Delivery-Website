10e670cf9086f79fef52bb1874f0b54da4ca6a35
ehamed <ehamed@ejada.com>
2025-06-30 14:14:54 +0300

    Implemented footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { NotificationBarComponent } from './shared/components/notification-bar/notification-bar.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, NotificationBarComponent, NotificationBarComponent],
+  imports: [RouterOutlet, NavbarComponent, NotificationBarComponent, NotificationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
